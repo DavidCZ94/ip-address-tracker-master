10e5362652321231645a6f0490c428114fdce27d
DavidCZ94 <david940103@gmail.com>
2021-10-03 22:59:05 -0500

    environments adjusted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  localIpApiUrl: 'https://api.ipify.org/?format=json',
+  ipifyGeolocationApi: 'https://geo.ipify.org/api/v1',
+  ipifyApiKey: 'at_oDelgQX6IcdQC9bUuXKt3FT0PO0zR'
 };
