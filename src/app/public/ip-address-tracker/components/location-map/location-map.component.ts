99010da253a7d690136957c060225e6ec1b252fc
DavidCZ94 <david940103@gmail.com>
2021-10-03 23:43:30 -0500

    makers addeds to map-component

--- src/app/public/ip-address-tracker/components/location-map/location-map.component.ts
+++ src/app/public/ip-address-tracker/components/location-map/location-map.component.ts
@@ -3,6 +3,21 @@ import { Observable } from 'rxjs';
 import { IpLocationService } from '../../shared/ip-location.service';
 import * as L from 'leaflet';
 
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 @Component({
   selector: 'app-location-map',
   templateUrl: './location-map.component.html',
@@ -36,7 +51,7 @@ export class LocationMapComponent implements OnInit, AfterViewInit {
    private initMap(lat: number, lng: number): void {
     this.map = L.map('map', {
       center: [ lat | 4.60971, lng | -74.08175],
-      zoom: 9
+      zoom: 10
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -44,6 +59,11 @@ export class LocationMapComponent implements OnInit, AfterViewInit {
       minZoom: 3,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
+
+    console.log('Lat=' + lat);
+    console.log('lng=' + lng);
+
+    L.marker([ lat | 4.60971, lng | -74.08175]).addTo(this.map);
 
     tiles.addTo(this.map);
   }
